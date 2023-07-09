09004340a7ccc1a082371b55c2f8874ea2e2f47e
Data Nerd <118627524+Sreecharan9@users.noreply.github.com>
2023-07-09 22:55:38 +0530

    Update Day_5.sql

--- Day_05 - Medium/Day_5.sql
+++ Day_05 - Medium/Day_5.sql
@@ -19,15 +19,15 @@ WHERE "rank" = 1
 Output:
 --------------------
 
-Business_name	Year	Inspection_score
-Castro Street Chevron	2015	100
-MARTIN L. KING MIDDLE SCHOOL	2015	100
-Taco Bell Cantina #31685	2016	100
-MV Taurus	2016	100
-Hilton Financial District- Restaurant Seven Fifty	2016	100
-Crepe and Brioche, Inc.	2016	100
-Yerba Buena Tea Co (formerly Tea Smiths of SF)	2016	100
-Project Juice	2016	100
-Bebebar Juice & Sandwich	2016	100
-Jersey	2017	100
-North Point Market	2018	100
+Business_name	                                                    Year	         Inspection_score
+Castro Street Chevron	                                            2015	              100
+MARTIN L. KING MIDDLE SCHOOL	                                    2015	              100
+Taco Bell Cantina #31685	                                        2016	              100
+MV Taurus	                                                        2016	              100
+Hilton Financial District- Restaurant Seven Fifty	                2016	              100
+Crepe and Brioche, Inc.	                                          2016	              100
+Yerba Buena Tea Co (formerly Tea Smiths of SF)	                  2016	              100
+Project Juice	                                                    2016	              100
+Bebebar Juice & Sandwich	                                        2016	              100
+Jersey	                                                          2017	              100
+North Point Market	                                              2018	              100
