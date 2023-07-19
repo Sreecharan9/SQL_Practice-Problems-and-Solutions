93289062475438aaa568acdefd4a66b858c2f331
Data Nerd <118627524+Sreecharan9@users.noreply.github.com>
2023-07-19 18:50:38 +0530

    Update Day_19.sql

--- Day_19 - Medium/Day_19.sql
+++ Day_19 - Medium/Day_19.sql
@@ -4,8 +4,8 @@ Query
 SELECT *
 FROM (
     SELECT 
-        UNNEST(STRING_TO_ARRAY(col1 || ',' || col2 || ',' || col3 || ',' || col4 || ',' || col5 || ',' || col6, ',')) AS newstring,
-        COUNT(*) AS count
+        UNNEST(STRING_TO_ARRAY(col1 || ',' || col2 || ',' || col3 || ',' || col4 || ',' || col5 || ',' || col6, ',')) AS newstring
+       ,COUNT(*) AS count
     FROM 
         alphabets
     GROUP BY 1
