c0fb51e9195ebf357d4a810fd43ae3ba3e602ea1
Data Nerd <118627524+Sreecharan9@users.noreply.github.com>
2023-07-19 17:10:30 +0530

    Update Day_19.sql

--- Day_19 - Medium/Day_19.sql
+++ Day_19 - Medium/Day_19.sql
@@ -6,12 +6,13 @@ SELECT *
 FROM
 (SELECT 
 	UNNEST(STRING_TO_ARRAY(col1||','||col2||','||col3||','||col4||','||col5||','||col6,',')) as newstring
-    ,COUNT(*)
+	,COUNT(*)
 FROM 
 	alphabets
 GROUP BY 1
 ) as words
-WHERE newstring = 'G'
+WHERE 
+	newstring = 'G'
 
 
 
