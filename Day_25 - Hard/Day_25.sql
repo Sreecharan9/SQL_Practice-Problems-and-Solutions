e5101fe719e15977094708e28f0b0837f56b19f3
Data Nerd <118627524+Sreecharan9@users.noreply.github.com>
2023-07-25 23:43:46 +0530

    Update Day_25.sql

--- Day_25 - Hard/Day_25.sql
+++ Day_25 - Hard/Day_25.sql
@@ -5,13 +5,14 @@ Query
 WITH retention AS
 (SELECT 
     to_char(order_date,'MM') as Month_Num
-    ,CASE WHEN EXTRACT('month' FROM order_date)-EXTRACT('month' FROM LAG(order_date,1,order_date) OVER(PARTITION BY cust_id ORDER BY order_id)) = 0 THEN NULL ELSE cust_id END as earlier_order_Cust
+    ,CASE WHEN EXTRACT('month' FROM order_date)-EXTRACT('month' FROM LAG(order_date,1,order_date) OVER(PARTITION BY cust_id ORDER BY order_id)) = 0 THEN NULL 
+	  ELSE cust_id END as earlier_order_Cust
 FROM transactions
 )
 
 SELECT 
 	month_num
-    ,COUNT(earlier_order_cust)
+	,COUNT(earlier_order_cust)
  FROM retention
  GROUP BY 1
 
