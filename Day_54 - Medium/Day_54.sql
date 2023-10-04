b0dafb8745bdf519aab084761f8f703400993f2f
Data Nerd <118627524+Sreecharan9@users.noreply.github.com>
2023-10-04 17:26:19 +0530

    Create Day_54.sql

---
+++ Day_54 - Medium/Day_54.sql
@@ -0,0 +1,22 @@
+-------------------
+Query
+-------------------
+
+SELECT 
+    id
+    ,CASE WHEN MOD(id,2)<> 0 THEN LEAD(student,1,student) OVER() 
+          WHEN MOD(id,2) = 0 THEN LAG(student,1,student) OVER() END AS student
+FROM 
+    seats_table
+
+--------------------
+Output
+--------------------
+
+|  ID  | Student |
+|------|---------|
+|  1   |    D    |
+|  2   |    A    |
+|  3   |    G    |
+|  4   |    E    |
+|  5   |    J    |
