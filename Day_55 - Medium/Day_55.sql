a4e5cbf91354c205ec65af7e03ae1aa328bead71
Data Nerd <118627524+Sreecharan9@users.noreply.github.com>
2023-10-08 11:46:39 +0530

    Update Day_55.sql

--- Day_55 - Medium/Day_55.sql
+++ Day_55 - Medium/Day_55.sql
@@ -22,3 +22,12 @@ WHERE MOD(rn,2) <> 0
 -------------------
 Output
 -------------------
+
+|        names         |
+|----------------------|
+| 1 Person1, 2 Person2 |
+| 3 Person3, 4 Person4 |
+| 5 Person5, 6 Person6 |
+| 7 Person7, 8 Person8 |
+| 9 Person9, 10 Person10|
+
