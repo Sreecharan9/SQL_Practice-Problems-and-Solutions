c56f4c2fce4ad2f53a1dff2a2ba00ae4b3c6d5fd
Data Nerd <118627524+Sreecharan9@users.noreply.github.com>
2023-11-08 18:09:10 +0530

    Create Day_61.sql

---
+++ Day_61 - Medium/Day_61.sql
@@ -0,0 +1,31 @@
+--------------------
+Query
+--------------------
+
+WITH a1 as
+(
+SELECT *,
+  	ROW_NUMBER() OVER() as rn
+FROM 
+  	Teams
+)
+
+SELECT 
+	A.Team || ' VS ' || B.Team as Matches
+FROM 
+	a1 A
+INNER JOIN 
+	a1 B 
+ON A.rn < B.rn
+
+-------------------
+Output
+-------------------
+
+|            Matches            |
+|-----------------------------|
+|     India VS Pakistan     |
+|    India VS Sri Lanka    |
+|    India VS Australia    |
+| Pakistan VS Sri Lanka |
+| Pakistan VS Australia |
